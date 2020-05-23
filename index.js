4063e8a5bc4eac1955f7609ac484286eae56b99e
Jordan <jordanleelong5@gmail.com>
2020-05-22 19:52:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 animal = ""
+// above was code already given, it is a "global variable"
 
 function myAnimal() {
+  // myAnimal function needed animal to be defined in this functions scope
+  // then return animal was already given
   let animal = "dog";
   return animal
 }
 
 function yourAnimal() {
+  // kind of the same thing, needed an animal variable to be defined in this functions scope
+  // return animal was code already given
   let animal = "cat";
   // How can we make sure that this function
   // and the above function both pass?
@@ -14,6 +19,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  // needed variable two to be defined inside the functions scope
+  // return n + two was already given
   let two = 2;
   return n + two
 
